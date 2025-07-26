b0b98a3e991f6fdb4604665407369a1e0370a000
Guilherme <gusantos.ok@gmail.com>
2025-07-26 03:52:09 -0300

    v3

--- connection.py
+++ connection.py
@@ -1,28 +1,21 @@
-import tomllib
-import base64
+import streamlit as st
 from databricks import sql
 
-def load_config(path="config.toml"):
-    with open(path, "rb") as f:
-            return tomllib.load(f)
-    
-
-config = load_config().get('database')
 
 connection = sql.connect(
-    server_hostname=config.get('HOSTNAME'),
-    http_path=config.get('HOSTPATH'),
-    access_token=config.get('HOSTTOKEN'),
+    server_hostname=st.secrets.database.HOSTNAME,
+    http_path=st.secrets.database.HOSTPATH,
+    access_token=st.secrets.database.HOSTTOKEN,
 )
 
 def get_state():
     with connection.cursor() as cursor:
-        cursor.execute(f"SELECT state FROM {config.get('TABLENAME')}")
+        cursor.execute(f"SELECT state FROM {st.secrets.database.TABLENAME}")
         result = cursor.fetchall()
         return result[0].asDict()['state']
 
 def set_state(state):
     with connection.cursor() as cursor:
-        cursor.execute(f"UPDATE {config.get('TABLENAME')} SET state = ?", (state,))
+        cursor.execute(f"UPDATE {st.secrets.database.TABLENAME} SET state = ?", (state,))
         connection.commit()
         return True
